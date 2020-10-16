ce8c247d276b5eba1da8745f2903ada279ddf91c
liubao <bismy@qq.com>
2020-10-16 17:14:45 +0800

    [#40]存在 autowire 的场景， 会将 consumer 注册为 endpoints

--- discovery/servicecomb-service-center/src/main/java/com/huaweicloud/dubbo/discovery/RegistrationListener.java
+++ discovery/servicecomb-service-center/src/main/java/com/huaweicloud/dubbo/discovery/RegistrationListener.java
@@ -62,6 +62,8 @@ import com.huaweicloud.dubbo.common.RegistrationReadyEvent;
 import com.huaweicloud.dubbo.common.SchemaInfo;
 
 public class RegistrationListener implements ApplicationListener<ApplicationEvent>, ApplicationEventPublisherAware {
+  private static final String PROTOCOL_CONSUMER = "consumer";
+
   static class SubscriptionKey {
     final String appId;
 
@@ -178,7 +180,10 @@ public class RegistrationListener implements ApplicationListener<ApplicationEven
         instance = ServiceCenterConfiguration.createMicroserviceInstance();
         List<String> endpoints = new ArrayList<>();
         if (registry != null) {
-          endpoints.addAll(registry.getRegisters().stream().map(URL::toString).collect(Collectors.toList()));
+          endpoints.addAll(registry.getRegisters().stream()
+              .filter(item -> !item.getProtocol().equals(PROTOCOL_CONSUMER))
+              .map(URL::toString)
+              .collect(Collectors.toList()));
         }
         instance.setEndpoints(endpoints);
         instance.setHostName(InetAddress.getLocalHost().getHostName());
@@ -197,7 +202,7 @@ public class RegistrationListener implements ApplicationListener<ApplicationEven
     } else if (applicationEvent instanceof NewSubscriberEvent) {
       NewSubscriberEvent newSubscriberEvent = (NewSubscriberEvent) applicationEvent;
       // 第一次订阅， 按照 dubbo 的要求， 需要查询实例列表
-      if (newSubscriberEvent.getUrl().getProtocol().equals("consumer")) {
+      if (newSubscriberEvent.getUrl().getProtocol().equals(PROTOCOL_CONSUMER)) {
         if (registrationInProgress) {
           pendingSubscribeEvent.add(newSubscriberEvent);
           return;
